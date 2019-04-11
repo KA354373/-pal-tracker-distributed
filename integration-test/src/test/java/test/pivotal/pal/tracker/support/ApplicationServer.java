8da78f1a74d194f54336666e23f0e4773988420c
karthick <karthick.p61@wipro.com>
2019-04-11 13:56:32 -0700

    10th Lab Initial commit

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -36,10 +36,12 @@ public class ApplicationServer {
         String dbUrl = "jdbc:mysql://localhost:3306/" + dbName + "?useSSL=false&useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false";
 
         start(envMapBuilder()
-            .put("SPRING_DATASOURCE_URL", dbUrl)
-            .put("REGISTRATION_SERVER_ENDPOINT", "http://localhost:8883")
-            .build()
-        );
+                .put("SPRING_DATASOURCE_URL", dbUrl)
+                .put("EUREKA_CLIENT_ENABLED", "false")
+                .put("RIBBON_EUREKA_ENABLED", "false")
+                .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
+                .build()
+      );
     }
 
     public void stop() {
@@ -53,7 +55,7 @@ public class ApplicationServer {
 
     private static void waitUntilServerIsUp(String port) throws InterruptedException {
         HttpClient httpClient = new HttpClient();
-        int timeout = 120;
+        int timeout = 1000;
         Instant start = Instant.now();
         boolean isUp = false;
 
